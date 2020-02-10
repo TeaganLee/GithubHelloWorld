aaf974e5ab2f9e4cff7dc3b3861bf1a75d78b005
Teagan Lee <teagan.lee.2020@student.lhschools.org>
2020-02-10 13:01:30 -0500

    called Hello World Method again

--- src/GithubHelloWorld.java
+++ src/GithubHelloWorld.java
@@ -3,13 +3,17 @@ public class GithubHelloWorld {
 
         System.out.println("Hello World");
         helloWorld();
+        yourName();
     }
 
     public static void helloWorld(){
 
         System.out.println("Hello Github World");
     }
+    public static void yourName(){
 
+        System.out.println("This is Teagan Lee.");
+    }
 
 
 
